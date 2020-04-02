3bf345db3690cfa4874a294cdefc5a8062d0279b
Elite <Elite@DESKTOP-0L03IAF>
2020-04-02 09:18:27 +0200

    Closes #1 Create authorization

---
+++ RenameLater/models/response/LoggedUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace RenameLater.models.response
+{
+    public class LoggedUser
+    {
+        [JsonProperty(Required = Required.Always)]
+        public string Token { get; set; }
+
+        [DefaultValue(null)]
+        public string Email { get; set; }
+
+        [DefaultValue(null)]
+        public string LastName { get; set; }
+
+        [DefaultValue(null)]
+        public string FirstName { get; set; }
+    }
+}
