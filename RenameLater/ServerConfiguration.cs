8ba1769350cdef744fdcf9ada92806592fd93813
Elite <Elite@DESKTOP-0L03IAF>
2020-04-02 07:58:32 +0200

    Closes #4 Add HttpRequests class for communication with rest api

---
+++ RenameLater/ServerConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RenameLater
+{
+    public static class ServerConfiguration
+    {
+        public static string Url = "https://bookcrossingbackend-dev-as.azurewebsites.net/";
+        public static string Login = "api/Login";
+    }
+}
