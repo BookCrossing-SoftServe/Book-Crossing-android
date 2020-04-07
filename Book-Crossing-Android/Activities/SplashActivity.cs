b526e07070aefb53039942dcb1845852e7e87610
Elite <Elite@DESKTOP-0L03IAF>
2020-04-07 11:45:28 +0200

    Closes #9 - Change login UI design

--- Book-Crossing-Android/Activities/SplashActivity.cs
+++ Book-Crossing-Android/Activities/SplashActivity.cs
@@ -16,16 +16,30 @@ namespace Book_Crossing_Android.Activities
     [Activity(Label = "SplashActivity",Theme = "@style/MyTheme.splash",MainLauncher = true,NoHistory = true,ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
     public class SplashActivity : AppCompatActivity
     {
+        private ISharedPreferences _sharedPreferences;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
+            _sharedPreferences = Application.Context.GetSharedPreferences("userInfo", FileCreationMode.Private);
+            string firstName = _sharedPreferences.GetString("firstName", string.Empty);
+            string lastName = _sharedPreferences.GetString("lastName", string.Empty);
+            string email = _sharedPreferences.GetString("email", string.Empty);
+            if (firstName == string.Empty && lastName == string.Empty)
+            {
+                StartActivity(typeof(LoginActivity));
+            }
+            else
+            {
+                Intent intent = new Intent(this, typeof(MainActivity));
+                intent.PutExtra("firstName", firstName);
+                intent.PutExtra("lastName", lastName);
+                intent.PutExtra("email", email);
+                StartActivity(intent);
+            }
         }
-        protected override void OnResume()
-        {
-            base.OnResume();
-            StartActivity(typeof(LoginActivity));
-        }
+        //  private void RetrieveData()
     }
 }
